db269ab82b97274d931ee50c91d5758074c69cc9
EastTexasElectronics <Contact@EastTexaselectronics.com>
2023-04-25 11:51:07 -0500

    init commit

--- remix.config.js
+++ remix.config.js
@@ -17,9 +17,11 @@ module.exports = {
   serverPlatform: 'neutral',
   serverMinify: process.env.NODE_ENV === 'production',
   future: {
+    unstable_postcss: true,
+    unstable_tailwind: true,
     v2_meta: true,
-    v2_errorBoundary: true,
     v2_routeConvention: true,
+    v2_errorBoundary: true,
     v2_normalizeFormMethod: true,
   },
 };
